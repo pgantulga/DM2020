a0989773f7f0b8f39a77f4f87f281174a4cea4e1
tulgaaa <pggan2@gmail.com>
2020-10-09 16:02:06 +0800

    regbackend

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,29 @@
 import * as functions from 'firebase-functions';
+// @ts-ignore
+import * as admin from 'firebase-admin';
+
+admin.initializeApp();
+
+exports.invoiceNumber = functions.firestore
+  .document('orders/{orderId}')
+  .onCreate((snap: any, context: any) => {
+    const docRef = admin.firestore().collection('orders').doc(context.params.orderId);
+    admin.firestore().collection('orders').orderBy('createdAt', 'desc').limit(2)
+      .get()
+      .then((snapshot: any) => {
+        snapshot.forEach((item: any) => {
+          if (item.data().invoiceNumber) {
+            const latestInvoiceNumber = item.data().invoiceNumber + 1;
+            docRef.update({
+              invoiceNumber: latestInvoiceNumber
+            })
+              .then(() => {
+                console.log(latestInvoiceNumber);
+              });
+          }
+        });
+      });
+  })
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
